c5258a2ec5059b90b4064e9075c3f453a1823b4c
Jakkaphan Payuhakait <tee.payuhakait@thoughtworks.com>
2022-01-04 17:40:06 +0700

    :sparkles: Add fonts constant

---
+++ src/constants/app-fonts.ts
@@ -0,0 +1,34 @@
+export default {
+  regular: {
+    name: 'prompt-regular',
+    location: require('../assets/fonts/Prompt-Regular.ttf'),
+  },
+  italic: {
+    name: 'prompt-italic',
+    location: require('../assets/fonts/Prompt-Italic.ttf'),
+  },
+  light: {
+    name: 'prompt-light',
+    location: require('../assets/fonts/Prompt-Light.ttf'),
+  },
+  lightItalic: {
+    name: 'prompt-light-italic',
+    location: require('../assets/fonts/Prompt-LightItalic.ttf'),
+  },
+  semiBold: {
+    name: 'prompt-semi-bold',
+    location: require('../assets/fonts/Prompt-SemiBold.ttf'),
+  },
+  semiBoldItalic: {
+    name: 'prompt-semi-bold-italic',
+    location: require('../assets/fonts/Prompt-SemiBoldItalic.ttf'),
+  },
+  bold: {
+    name: 'prompt-bold',
+    location: require('../assets/fonts/Prompt-Bold.ttf'),
+  },
+  boldItalic: {
+    name: 'prompt-bold-italic',
+    location: require('../assets/fonts/Prompt-BoldItalic.ttf'),
+  },
+}
